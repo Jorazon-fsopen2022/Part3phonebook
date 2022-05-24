035566a22a3ed3b67e4b380b24a7433b79c3aabd
Jorazon <oskari.pahkala@gmail.com>
2022-05-24 15:27:34 +0300

    serve static page

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("build"));
 
 morgan.token("body", (request, response) => {
 	return JSON.stringify(request.body);
